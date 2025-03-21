32ca8a6b55741bcfb1cf75cfb00d63a59fe68ca1
Algebar347 <kwu7@outlook.com>
2025-03-22 00:04:03 +0800

    blankrow

--- main.py
+++ main.py
@@ -102,15 +102,16 @@ class MyPlugin(Star):
 
         found_characters = get_characters_by_name(name)
         if found_characters:
-            character_info = "\n".join([
-                (f"{i.full_name}" if i.full_name else "") +
-                (f"{i.role}" if i.role else "") +
-                (f"{i.clue1}" if i.clue1 else "") +
-                (f"{i.clue2}" if i.clue2 else "") +
-                (f"{i.clue3}" if i.clue3 else "")
-                for i in found_characters
-            ])
-            #character_info=f"{found_characters.full_name}\n{found_characters.role}\n{found_characters.clue1}\n{found_characters.clue2}\n{found_characters.clue3}"
+            character_info = "\n\n".join([  # 每个角色信息之间插入两个换行
+                 "\n".join(filter(None, [  # 过滤掉空字段
+                     i.full_name,
+                     i.role,
+                     i.clue1,
+                     i.clue2,
+                     i.clue3]
+                     ))
+                       for i in found_characters
+                       ])
             yield event.plain_result(f"人物: {character_info}")
         else:
             found_allcharacters = self.session.query(self.Character.name).all()
